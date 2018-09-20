1aae560bfd4977620f7f2ed77ef857cbff2fed70
Son Trinh Bao <email@sample.com>
2018-09-20 21:59:30 +0700

    Init project

---
+++ Son_HelloGit/Hello_Git/Hello_Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello_Git
+//
+//  Created by Thuy Truong Quang on 9/20/18.
+//  Copyright © 2018 Thuy Truong Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
