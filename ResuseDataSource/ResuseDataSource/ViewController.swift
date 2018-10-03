44c80a9e9ba8d10bd2c0e4d5b97cc21fbb523ffa
Thuy Truong Quang <thuytq@fsoft.com.vn>
2018-10-03 13:58:11 +0700

    update guide line.

--- ResuseDataSource/ResuseDataSource/ViewController.swift
+++ ResuseDataSource/ResuseDataSource/ViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    /*
+     Examp1: TableView using data of muplite arrays.
+     */
     var data = [
         [0,1,2,3,4,5],
         [6,7,8,9]
     ]
-    var dataSources: SectionedTableViewDataSource?
     
+    /*
+     Examp2: TableView using data of array.
+     */
+    
+    
+    var dataSimple = [10, 11, 12, 13, 14, 15]
+    var dataSources: SectionedTableViewDataSource?
+    var dataSource: UITableViewDataSource?
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +35,8 @@ class ViewController: UIViewController {
     func loadData() {
         dataSources = SectionedTableViewDataSource(dataSources: preapareTableViewDataSource(data: data))
         
-        tableView.dataSource = dataSources
+        dataSource = TableViewDataSource.make(for: dataSimple)
+        tableView.dataSource = dataSource
     }
     
     func preapareTableViewDataSource(data: [[Int]]) -> [UITableViewDataSource] {
